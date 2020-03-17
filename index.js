3181ab99b5d79ebeefb508469eff6d64862c81b7
Damika Weems <sweetcaramel0619@gmail.com>
2020-03-17 18:25:15 -0400

    Baby contructor passing

--- index.js
+++ index.js
@@ -73,9 +73,31 @@ Person.prototype.toString = function() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0
 }
+
+Car.prototype.fill = function(gallons) {
+  return this.tank += gallons;
+}
+
+Car.prototype.drive = function(distance) {
+  const gallons = distance / this.milesPerGallon;
+    if(this.tank >= gallons) {
+      this.odometer += distance; this.tank -= gallons
+    } else {
+      this.tank = 0;
+      this.odometer += gallons; 
+    }
+}
+
+Car.prototype.fuel = function() {
+  return `I ran out of fuel at ${this.odometer} miles!`
+}
+
 
 /*
   TASK 3
@@ -84,8 +106,15 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy
+}
 
+Baby.prototype = Object.create(Person.prototype)
+
+Baby.prototype.play = function(favoriteToy) {
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
